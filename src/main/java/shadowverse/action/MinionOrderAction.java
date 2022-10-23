c03e06db57689ef0b16acf031562bcf4e6d77a48
石涵澎 <1535193592@qq.com>
2022-10-23 12:23:45 +0800

    Update MinionOrderAction.java

--- src/main/java/shadowverse/action/MinionOrderAction.java
+++ src/main/java/shadowverse/action/MinionOrderAction.java
@@ -5,13 +5,16 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import shadowverse.orbs.Minion;
 
+import java.util.Objects;
+
 public class MinionOrderAction extends AbstractGameAction {
 
-    private String orbID = null;
+    private String orbID;
 
     public MinionOrderAction() {
         this.actionType = ActionType.SPECIAL;
         this.attackEffect = AttackEffect.NONE;
+        this.orbID = "";
     }
 
     public MinionOrderAction(String orbID) {
@@ -24,9 +27,11 @@ public class MinionOrderAction extends AbstractGameAction {
     public void update() {
         AbstractPlayer p = AbstractDungeon.player;
         for (int i = 0; i < p.orbs.size(); i++) {
-            if (p.orbs.get(i) instanceof Minion && (this.orbID == null || p.orbs.get(i).ID.equals(this.orbID))) {
-                ((Minion) p.orbs.get(i)).order();
-                break;
+            if (p.orbs.get(i) instanceof Minion) {
+                if (Objects.equals(this.orbID, "") || Objects.equals(p.orbs.get(i).ID,this.orbID)) {
+                    ((Minion) p.orbs.get(i)).order();
+                    break;
+                }
             }
         }
         this.isDone = true;
